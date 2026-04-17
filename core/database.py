f3472deffb2393dc86d7d379f97be42271bdb22e
gjpalzamora <gjpalzamora@gmail.com>
2026-04-17 18:18:51 -0500

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,1 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
+# Se utiliza variable de entorno para seguridad según RNF4.2.1 [cite: 187, 188]
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://usuario:password@localhost:5432/enmilla_db")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
