ce0c3f035a700a852c3eefc5441dffc317c4137e
gjpalzamora <gjpalzamora@gmail.com>
2026-04-19 19:16:27 -0500

    Update database.py

--- database.py
+++ database.py
@@ -3,22 +3,25 @@ from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 
 def conectar_hoja():
-    # Definimos los permisos
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    
-    # Cargamos las credenciales desde los Secrets de Streamlit por seguridad
-    # Esto evita que sus claves queden expuestas en GitHub
+    # Usamos el nombre exacto que pusimos en los Secrets
     creds_dict = st.secrets["gcp_service_account"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    
     client = gspread.authorize(creds)
-    # Debe crear una hoja en su Google Drive llamada EXACTAMENTE: EnMilla_DB
+    # Verifique que su hoja en Drive se llame exactamente así
     return client.open("EnMilla_DB")
 
 def obtener_datos(pestaña):
-    sheet = conectar_hoja().worksheet(pestaña)
-    return sheet.get_all_records()
+    try:
+        sheet = conectar_hoja().worksheet(pestaña)
+        return sheet.get_all_records()
+    except:
+        return []
 
 def registrar_fila(pestaña, fila):
-    sheet = conectar_hoja().worksheet(pestaña)
-    sheet.append_row(fila)
+    try:
+        sheet = conectar_hoja().worksheet(pestaña)
+        sheet.append_row(fila)
+        return True
+    except:
+        return False
